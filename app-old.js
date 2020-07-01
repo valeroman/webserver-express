73e4c08d4b3152fb7e44a0034254885fe4390c73
Roman <valeroman@gmail.com>
2020-07-01 15:57:45 -0500

    Primer commit

---
+++ app-old.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+
+        let salida = {
+            nombre: 'Roman',
+            planeta: 'Tierra',
+            url: req.url
+        }
+        res.write(JSON.stringify(salida));
+        //res.write('Hola mundo');
+        res.end();
+
+    })
+    .listen(8080);
+
+
+console.log('Escuchando el puerto 8080');
